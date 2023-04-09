dba844c570ef8ef09332d94b7a86fe6c1812308a
kingjky@gmail.com <kyungyoon.jeon@nhnpayco.com>
2023-04-09 16:48:39 +0900

    first commit

---
+++ src/main/java/com/yeomisae/yeomisae/YeomisaeApplication.java
@@ -0,0 +1,13 @@
+package com.yeomisae.yeomisae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeomisaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeomisaeApplication.class, args);
+	}
+
+}
