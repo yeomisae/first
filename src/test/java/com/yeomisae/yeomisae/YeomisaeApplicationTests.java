e59d82b98339543b6aff8cdba79024f27007eb3a
kingjky@gmail.com <kyungyoon.jeon@nhnpayco.com>
2023-04-09 17:10:36 +0900

    init

---
+++ src/test/java/com/yeomisae/yeomisae/YeomisaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeomisae.yeomisae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeomisaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
